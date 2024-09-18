db42a6c0a9d3a2e296bbea4772cc85e6a6e73f36
Ishaan Bhagat <ishaanbhagat@princeton.edu>
2024-09-17 21:27:08 -0400

    Seems to be working

---
+++ Project2/mw0785_ib5506_sy8603/COS-ECE470-fa2024-main/rustlings_exercises/vecs2.rs
@@ -0,0 +1,58 @@
+// vecs2.rs
+//
+// A Vec of even numbers is given. Your task is to complete the loop so that
+// each number in the Vec is multiplied by 2.
+//
+// Make me pass the test!
+// Hints at the bottom.
+
+fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    for element in v.iter_mut() {
+        *element *= 2;
+    }
+
+    // At this point, `v` should be equal to [4, 8, 12, 16, 20].
+    v // if no semicolon, the last expression is implicitely returned
+}
+
+fn vec_map(v: &Vec<i32>) -> Vec<i32> {
+    v.iter().map(|element| {
+        element * 2
+    }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vec_loop() {
+        let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let ans = vec_loop(v.clone());
+
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    }
+
+    #[test]
+    fn test_vec_map() {
+        let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        let ans = vec_map(&v);
+
+        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    }
+}
+
+
+
+
+
+
+
+// In the first function we are looping over the Vector and getting a reference to one `element` at a time.
+// To modify the value of that `element` we need to use the * dereference operator. You can learn more in this chapter of the Rust book:
+// https://doc.rust-lang.org/stable/book/ch08-01-vectors.html#iterating-over-the-values-in-a-vector
+
+// In the second function this dereferencing is not necessary, because the map function expects the new value to be returned.
+
+// After you've completed both functions, decide for yourself which approach you like better.
+// What do you think is the more commonly used pattern under Rust developers?
