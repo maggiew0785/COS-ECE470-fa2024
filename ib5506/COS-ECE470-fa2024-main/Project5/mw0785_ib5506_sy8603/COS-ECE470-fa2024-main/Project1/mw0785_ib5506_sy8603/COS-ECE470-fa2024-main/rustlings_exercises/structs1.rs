d6422b66209cdfb9353c48ee8f9eb9b2657662b3
Ishaan Bhagat <ishaanbhagat@princeton.edu>
2024-10-27 20:12:27 -0400

    Much more?

---
+++ ib5506/COS-ECE470-fa2024-main/Project5/mw0785_ib5506_sy8603/COS-ECE470-fa2024-main/Project1/mw0785_ib5506_sy8603/COS-ECE470-fa2024-main/rustlings_exercises/structs1.rs
@@ -0,0 +1,64 @@
+// structs1.rs
+//
+// Address all the TODOs to make the tests pass!
+
+struct ColorClassicStruct {
+    red: i32, 
+    green: i32,
+    blue: i32,
+}
+
+struct ColorTupleStruct(i32, i32, i32);
+
+#[derive(Debug)]
+struct UnitLikeStruct;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn classic_c_structs() {
+        // Instantiate a classic c struct!
+        let green = ColorClassicStruct {
+            red: 0,
+            green: 255,
+            blue: 0
+        };
+
+        assert_eq!(green.red, 0);
+        assert_eq!(green.green, 255);
+        assert_eq!(green.blue, 0);
+    }
+
+    #[test]
+    fn tuple_structs() {
+        // Instantiate a tuple struct!
+        let green = ColorTupleStruct(0, 255, 0);
+
+        assert_eq!(green.0, 0);
+        assert_eq!(green.1, 255);
+        assert_eq!(green.2, 0);
+    }
+
+    #[test]
+    fn unit_structs() {
+        // Instantiate a unit-like struct!
+        let unit_like_struct = UnitLikeStruct;
+        let message = format!("{:?}s are fun!", unit_like_struct);
+
+        assert_eq!(message, "UnitLikeStructs are fun!");
+    }
+}
+
+
+
+// Rust has more than one type of struct. Three actually, all variants are used to package related data together.
+// There are normal (or classic) structs. These are named collections of related data stored in fields.
+// Tuple structs are basically just named tuples.
+// Finally, Unit-like structs. These don't have any fields and are useful for generics.
+
+// In this exercise you need to complete and implement one of each kind.
+// Read more about structs -
+// The Book: https://doc.rust-lang.org/book/ch05-01-defining-structs.html
+// Rust by Example : https://doc.rust-lang.org/rust-by-example/custom_types/structs.html 
