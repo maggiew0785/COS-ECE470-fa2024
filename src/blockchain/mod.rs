1c811d3ca4b6028f3e15734d0359aed574696b22
Ishaan Bhagat <ishaanbhagat@princeton.edu>
2024-10-27 20:30:08 -0400

    wha

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -22,7 +22,7 @@ impl Blockchain {
         let parent = H256::from([0u8; 32]); // Parent is all zeroes
         let nonce = 0;
         let difficulty = H256::from(hex!(
-            "000000000000000000000000ffffffffffffffffffffffffffffffffffffffff"
+            "0000000000000000000000000000000000000000ffffffffffffffffffffffff"
         ));
         let content = crate::types::block::Content {
             data: vec![], // Empty transactions
